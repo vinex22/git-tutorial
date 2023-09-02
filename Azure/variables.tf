14b30c91cb0d9f65c9f63f90490cb4e87c3171f2
vinex22 <vinex22@gmail.com>
2023-09-02 13:25:45 +0800

    v

--- Azure/variables.tf
+++ Azure/variables.tf
@@ -1,12 +1,14 @@
 variable "rg_name" {
     default = "example-resource"
-  
+  type = string
 }
 variable "location" {
-    default = "Southeasr Asia"
-  
+    default = "Southeast Asia"
+  type = string
 }
 
 variable "net_name" {
   default = "example-network"
+  type = string
+
 }
